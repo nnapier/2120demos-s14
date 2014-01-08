80fcdac2f1456fe76a88a6183982f866f08ed3f7
nnapier <nnapier@ggc.edu>
2014-01-08 10:51:35 -0500

    January 8th code
    Simple class

---
+++ Code before Test 1/Simple.java
@@ -0,0 +1,11 @@
+// Nannette Napier: This is a simple Java program
+
+public class Simple
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Programming is great fun!");
+    System.out.println("My name is Nannette Napier.");
+    System.out.println("My major is IT.");
+  }
+}
