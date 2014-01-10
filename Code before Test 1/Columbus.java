55f486c7770b038c49764214634958f89256374f
nnapier <nnapier@ggc.edu>
2014-01-10 10:52:25 -0500

    Code from Fri, Jan 10
    POTD - Columbus
    Example using VariableExample - how to declare variables

---
+++ Code before Test 1/Columbus.java
@@ -0,0 +1,11 @@
+// A crazy mixed up program
+
+public class Columbus
+{
+  public static void main(String[] args)
+  {
+    System.out.println("In 1492 Columbus sailed the ocean blue.");
+    System.out.println("Hello, world!");
+    System.out.println("Third statement"); 
+  }
+}
---
+++ Code before Test 1/VariableExample.java
@@ -0,0 +1,20 @@
+// Example using variables
+
+public class VariableExample
+{
+  public static void main(String[] args)
+  {
+    // 1) Declare a variable that is of int
+    //    data type which is named num.
+    //    Initialize the value to 27.
+    int num = 27;
+    
+    // 2) Declare a variable that is of int
+    //    data type which is named width.
+    //    Initialize the value to 50.
+    int width = 50;
+    
+    // SOP the width 
+    System.out.println(width);System.out.println("Two times width");System.out.println(2*width);    System.out.println(num);
+  }
+}
