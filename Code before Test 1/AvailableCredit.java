9966c3d8716769939a9634ca4e1a8577e1b6c1eb
nnapier <nnapier@ggc.edu>
2014-01-13 10:52:16 -0500

    Jan 13 changes

---
+++ Code before Test 1/AvailableCredit.java
@@ -0,0 +1,13 @@
+// Your name here
+public class AvailableCredit
+{
+  public static void main(String[] args)
+  {
+    double maxCredit = 1000.0;
+    double creditUsed = 23.25;
+    
+    System.out.println("Maximum credit is " + maxCredit);
+    
+  }
+    
+}
---
+++ Code before Test 1/Initial.java
@@ -0,0 +1,12 @@
+public class Initial
+{
+  public static void main(String[] args)
+  {
+    int month = 2;
+    int days = 28;
+    System.out.println("Month " + month);
+    //System.out.print("Month " );
+    //System.out.println( month );
+    System.out.println(" has " + days + " Days. ");
+  }
+}
