dabd687bca8bfb59ede324959d10308eba683d38
nnapier <nnapier@ggc.edu>
2014-01-17 10:45:31 -0500

    In class code - Jan 17

---
+++ Code before Test 1/ExampleNewline.java
@@ -0,0 +1,14 @@
+public class ExampleNewline
+{
+  public static void main(String[] args)
+  {
+    double foodSubtotal = 50.0;
+    double foodTax = .06 * foodSubtotal;
+    
+    
+    System.out.println("First line.\nSecond line.\nThird line.");
+    
+
+                       
+  }
+}
