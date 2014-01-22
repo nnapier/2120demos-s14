551ccdbd41092e6044acf6ab8692b245901ea638
nnapier <nnapier@ggc.edu>
2014-01-22 10:17:58 -0500

    Example Jan 22

---
+++ Code before Test 1/DialogExample.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+
+public class DialogExample
+{
+  public static void main(String[] args)
+  {
+    String input = JOptionPane.showInputDialog("Enter an input");
+    System.out.println("They entered " + input);
+    
+    // Add another pop-up that asks about the age
+    String ageString = JOptionPane.showInputDialog("Please enter your age");
+    System.out.println("The age is " + ageString);
+    
+    // Convert from a String --> Int
+    int age = Integer.parseInt(ageString);
+    System.out.println("The age as an integer is: " + age);
+  }
+}
