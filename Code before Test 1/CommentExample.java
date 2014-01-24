7f46ca8398759864eac3a02b852970497b0b14f9
nnapier <nnapier@ggc.edu>
2014-01-24 10:44:05 -0500

    Friday Jan 24 changes
    CommentExample and new version of DialogExample

---
+++ Code before Test 1/CommentExample.java
@@ -0,0 +1,35 @@
+
+
+
+
+
+/** Class: CommentExample
+  * @author Nannette Napier
+  * @version 1.0
+  * Course: ITEC 2120 Spring 2014
+  * Written: January 24, 2014
+  * 
+  * 
+  * This class provides an example of different
+  * ways of commenting.
+  * 
+  * Purpose: Show commenting examples
+  */
+
+public class CommentExample
+{
+  public static void main(String[] args)
+  {
+     int num = 4;  // Initializing the num variable
+     double value; // Declaring a value
+     
+     // Now, time to get on with this program
+     System.out.println("My number is : " + num);
+     
+     /* I can use a block comment
+      * whenever I want to say 
+      * a lot of different things
+      * blah blah blah
+      */ 
+  }
+}
--- Code before Test 1/DialogExample.java
+++ Code before Test 1/DialogExample.java
@@ -4,15 +4,38 @@ public class DialogExample
 {
   public static void main(String[] args)
   {
+    String string = JOptionPane.showInputDialog( 
+                       null, "Enter a year:", "Example 2.2 Input (int)",
+                       JOptionPane.WARNING_MESSAGE);
+    // Use a message dialog box to show what they entered
+    JOptionPane.showMessageDialog( null,
+            "The year entered " + string,
+            "Display Message",
+            JOptionPane.INFORMATION_MESSAGE);
+                                  
+    //System.out.println("The year entered " + string);
+    
     String input = JOptionPane.showInputDialog("Enter an input");
-    System.out.println("They entered " + input);
+    //System.out.println("They entered " + input);
+    JOptionPane.showMessageDialog( null,
+            "They entered " + input,
+            "Display Message",
+            JOptionPane.INFORMATION_MESSAGE);
     
     // Add another pop-up that asks about the age
     String ageString = JOptionPane.showInputDialog("Please enter your age");
-    System.out.println("The age is " + ageString);
+    //System.out.println("The age is " + ageString);
+    JOptionPane.showMessageDialog( null,
+            "The age is " + ageString,
+            "Display Message",
+            JOptionPane.INFORMATION_MESSAGE);
     
     // Convert from a String --> Int
     int age = Integer.parseInt(ageString);
-    System.out.println("The age as an integer is:
+    //System.out.println("The age as an integer is: " + age);
+    JOptionPane.showMessageDialog( null,
+            "The age as an integer is: " + age,
+            "Display Message",
+            JOptionPane.INFORMATION_MESSAGE);
   }
 }
